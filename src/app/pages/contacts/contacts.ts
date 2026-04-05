a47ed564a1a718256bd53851c7ed83edb8c17e6e
Jewel Caudilla <jewelcaudilla@gmail.com>
2026-04-05 19:45:53 +0800

    Updated Contacts page.

--- src/app/pages/contacts/contacts.ts
+++ src/app/pages/contacts/contacts.ts
@@ -6,4 +6,39 @@ import { Component } from '@angular/core';
   templateUrl: './contacts.html',
   styleUrl: './contacts.scss',
 })
-export class Contacts {}
+export class Contacts {
+
+  contactInfos: Contact[] = [];
+
+  constructor() {
+    this.contactInfos = [
+      {
+        cardIcon: 'bi-telephone',
+        cardTitle: 'PHONE',
+        cardText: '+63-920-714-2897'
+      },
+      {
+        cardIcon: 'bi bi-envelope',
+        cardTitle: 'EMAIL',
+        cardText: 'jewelcaudilla@gmail.com'
+      },
+      {
+        cardIcon: 'bi bi-linkedin',
+        cardTitle: 'Linkedin',
+        cardText: 'www.linkedin.com/in/jewel-caudilla'
+      }
+    ];
+  }
+}
+
+class Contact {
+  cardIcon: string;
+  cardTitle: string;
+  cardText: string;
+
+  constructor() {
+    this.cardIcon = '';
+    this.cardTitle = '';
+    this.cardText = '';
+  }
+}
