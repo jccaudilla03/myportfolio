e00b98c881f99b615d372cd38d6df246b6d88f70
Jewel Caudilla <jewelcaudilla@gmail.com>
2026-04-06 15:20:14 +0800

    Updated Skills

--- src/app/pages/skills/skills.ts
+++ src/app/pages/skills/skills.ts
@@ -6,4 +6,41 @@ import { Component } from '@angular/core';
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
-export class Skills {}
+export class Skills {
+
+  skillsList: Skill[] = [];
+
+  constructor() {
+    this.skillsList = [
+      { name: 'Angular Framework', proficiency: 97 },
+      { name: 'Node.js', proficiency: 90 },
+      { name: '.NET Framework', proficiency: 70 },
+      { name: 'Agile Development', proficiency: 95 },
+      { name: 'Microsoft SQL Server', proficiency: 90 },
+      { name: 'PostgreSQL', proficiency: 85 },
+      { name: 'RESTful API', proficiency: 88 },
+      { name: 'CI/CD', proficiency: 98 },
+      { name: 'Azure DevOps', proficiency: 90 },
+      { name: 'Amazon Web Services', proficiency: 90 },
+      { name: 'Git', proficiency: 100 },
+      { name: 'Security compliance', proficiency: 90 },
+      { name: 'Technical leadership', proficiency: 90 },
+      { name: 'Problem solving', proficiency: 90 },
+      { name: 'Creative thinking', proficiency: 90 },
+      { name: 'Team collaboration', proficiency: 90 },
+      { name: 'Effective communication', proficiency: 90 },
+    
+    ];
+  }
+
+}
+
+class Skill {
+  name: string;
+  proficiency: number; // Percentage (0-100)
+
+  constructor() {
+    this.name = '';
+    this.proficiency = 0;
+  }
+}
